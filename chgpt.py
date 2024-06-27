f7c68539d09f326a784f7ac5e2ee561e2d45b9c7
stenli <s.nevrovsky@ya.ru>
2024-06-27 14:30:33 +0300

    Add chatgpt.....??????

---
+++ chgpt.py
@@ -0,0 +1,84 @@
+import requests
+from json import loads
+from time import time
+from config import MAX_SESSION_TIME, MAX_DOCTOR_TIME, CHATGPT_HOST, CHATGPT_KEY, CHATGPT_MODEL
+
+
+class ChGPT:
+    def __init__(self, api_key: str, url: str, model: str, prompt: str = False):
+        self.token = api_key
+        self.url = url
+        self.prompt = prompt
+        self.model = model
+        self.messages = []
+
+        if not prompt:
+            with open("prompt.txt", "r", encoding="UTF-8") as f:
+                self.set_prompt(f.read())
+
+        self.messages.append({"role": "system", "content": self.prompt})
+
+    def set_prompt(self, prompt: str):
+        self.prompt = prompt
+
+    def send_request(self, context: dict):
+        response = requests.post(self.url, json=context)
+        return response.text
+
+    def ask_model(self, question):
+        self.messages.append({"role": "user", "content": question})
+        payload = {"token": self.token,
+                   "model": self.model,
+                   "context": self.messages}
+        ans = loads(self.send_request(payload))
+        print(ans)
+        self.messages.append(ans["result"]["choices"][0]["message"]["content"])
+        return ans
+
+
+class ManyCGPT(ChGPT):
+    def __init__(self, api_key: str, url: str, model: str, prompt: str = False):
+        super().__init__(api_key, url, model, prompt)
+        self.agents = {}
+
+    def new_agent(self, contract_id):
+        self.agents[contract_id] = {"start_time": time(), "last_time": time(), "last_doc_time": 0,
+                                    "gpt": ChGPT(CHATGPT_KEY, CHATGPT_HOST, CHATGPT_MODEL)}
+
+    def clear_context(self, contract_id):
+        self.agents[contract_id] = {"start_time": time(), "last_time": time(), "last_doc_time": 0,
+                                    "gpt": ChGPT(CHATGPT_KEY, CHATGPT_HOST, CHATGPT_MODEL)}
+
+    def doc(self, contract_id):
+        self.agents[contract_id]["last_doc_time"] = time()
+
+    def is_dop_dop(self, contract_id):
+        if contract_id not in self.agents.keys():
+            return False
+        if time() - self.agents[contract_id]["last_doc_time"] < MAX_DOCTOR_TIME:
+            return True
+        return False
+
+    def ask(self, contract_id, question):
+        callback = "cont"
+        if contract_id not in self.agents.keys():
+            self.new_agent(contract_id)
+            callback = "new"
+        elif self.agents[contract_id] == 0:
+            self.new_agent(contract_id)
+            callback = "new"
+        elif time() - self.agents[contract_id]["last_time"] > MAX_SESSION_TIME:
+            self.new_agent(contract_id)
+            callback = "new"
+        elif time() - self.agents[contract_id]["last_doc_time"] < MAX_DOCTOR_TIME:
+            return {}, "dop"
+        res = self.agents[contract_id]["gpt"].ask_model(question)
+        self.agents[contract_id]["last_time"] = time()
+
+        return res, callback
+
+
+if __name__ == "__main__":
+    gpt = ManyCGPT(CHATGPT_KEY, CHATGPT_HOST, CHATGPT_MODEL)
+    print(gpt.ask("1111", "ЭЭЭ у меня башка балит"))
+    print(gpt.ask("1111", "А что мне тогида делать?"))
--- tasks.py
+++ tasks.py
@@ -3,13 +3,16 @@ from flask import Flask, request, render_template
 from config import *
 from medsenger_api import *
 from yagpt import YaGPT, ManyYGPT
+from chgpt import ManyCGPT
 from celery import Celery, Task
 import time
 from markdown2 import Markdown
 from infrastructure import celery
 
 ygpt = ManyYGPT(DIR_ID, API_KEY)
+cgpt = ManyCGPT(CHATGPT_KEY, CHATGPT_HOST, CHATGPT_MODEL)
 markdowner = Markdown()
+
 
 @celery.task
 def ask_yai(json: dict, txt: str) -> str:
@@ -24,6 +27,33 @@ def ask_yai(json: dict, txt: str) -> str:
     medsenger_api.send_message(json["contract_id"], "Запрос отправлен. Ожидаем ответ.", forward_to_doctor=False)
 
     ans, callback = ygpt.ask(json["contract_id"], txt)
+    print(ans, callback)
+    ai_ans = ans['result']['alternatives'][0]['message']['text']
+
+    if callback == "cont":
+        text = f"_Продолжение диалога._\n # Внимание! Ответ создан нейросетью, будьте осторожны, не принимайте всё за чистую монету!!\n{ai_ans}"
+    elif callback == "new":
+        text = f"_Новый диалог._\n # Внимание! Ответ создан нейросетью, будьте осторожны, не принимайте всё за чистую монету!!\n{ai_ans}"
+    else:
+        text = "# Что-то пошло не так."
+    html_ans = markdowner.convert(text).replace('\n', '')
+    medsenger_api.send_message(json["contract_id"], html_ans, forward_to_doctor=False)
+    return 1
+
+
+@celery.task
+def ask_cai(json: dict, txt: str) -> str:
+    if json["message"]["sender"] == "doctor":
+        cgpt.clear_context(json["contract_id"])
+        cgpt.doc(json["contract_id"])
+        return "ok"
+
+    medsenger_api = AgentApiClient(APP_KEY, MAIN_HOST, debug=True)
+    if cgpt.is_dop_dop(json["contract_id"]):
+        return 1
+    medsenger_api.send_message(json["contract_id"], "Запрос отправлен. Ожидаем ответ.", forward_to_doctor=False)
+
+    ans, callback = cgpt.ask(json["contract_id"], txt)
     print(ans, callback)
     ai_ans = ans['result']['alternatives'][0]['message']['text']
 
